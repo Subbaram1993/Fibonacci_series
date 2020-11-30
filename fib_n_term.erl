1315f9c89c38109c9c24553720d12984589813be
Subbaram1993 <74912140+Subbaram1993@users.noreply.github.com>
2020-11-30 17:14:25 +0530

    Create fib_n_term.erl

---
+++ fib_n_term.erl
@@ -0,0 +1,15 @@
+-module(prog).
+ 
+-export([fibonacci/1,fib/3]).
+ 
+fib(1,_,Res) -> 
+    Res;
+fib(N,Prev,Res) when N > 1 -> 
+    fib(N-1, Res, Res+Prev).
+ 
+fibonacci(M) -> 
+    X = 0,
+    Y = 1,
+    G = M,
+    F = fib(G,X,Y),
+    io:format("~nThe ~pth fibonacci number is ~p~n",[M,F]).
